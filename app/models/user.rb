43bf9cea7e8503aa279531492bf9b303aceb3d6e
simon-group <saku_crownheart@yahoo.co.jp>
2019-09-10 21:37:25 +0900

    save

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :videos
+  has_many :videos, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_videos, through: :likes, source: :video
   has_many :comments
+
+  def already_liked?(video)
+    self.likes.exists?(video_id: video.id)
+  end
 
   validates :name, presence: true,
   format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_071710) do
+ActiveRecord::Schema.define(version: 2019_09_10_112235) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_09_07_071710) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "video_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_likes_on_user_id"
+    t.index ["video_id"], name: "index_likes_on_video_id"
   end
 
   create_table "memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -52,4 +61,6 @@ ActiveRecord::Schema.define(version: 2019_09_07_071710) do
     t.integer "user_id"
   end
 
+  add_foreign_key "likes", "users"
+  add_foreign_key "likes", "videos"
 end
