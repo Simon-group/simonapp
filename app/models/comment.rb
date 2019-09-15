75142d6c20f70945580b79b42e842c7cd7ecfa70
simon-group <saku_crownheart@yahoo.co.jp>
2019-09-15 18:24:29 +0900

    syusei/css

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :video
   belongs_to :user
 
-  validates :text, presence: true, length: { minimun: 5,maximum: 30}
+  validates :text, presence: true, length: {maximum: 30}
 end
