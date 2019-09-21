91403f9f07b49e31ab37451ddb9bff638e53d97b
simon-group <saku_crownheart@yahoo.co.jp>
2019-09-22 02:25:39 +0900

    stylechange

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,32 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(comment){
+    var html = `
+                <div class ="username">
+                ${comment.user_name}
+                <div class="comment-style">
+                  ${comment.text}`
+    return html;
+  }
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href + '/comments'
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comments').append(html);
+      $('.message-field__area').val('')
+    })
+//↓エラー時の処理
+    .fail(function(){
+      alert("error");
+    })
+  })
+})
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(text: comment_params[:text], video_id: comment_params[:video_id], user_id: current_user.id)
-    redirect_to "/videos/#{comment.video.id}"   #コメントと結びつくツイートの詳細画面に遷移する
+    @comment = Comment.create(text: comment_params[:text], video_id: comment_params[:video_id], user_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to video_path(params[:video_id])  }
+      format.json
+    end
   end
 
   private
